64ff430b8ba73a530c15d0013d071860b07612bd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-04-23 21:57:11 +0200

    Return NoBody parser in case the factory can't find out which parser to use

--- src/FormParserFactory.php
+++ src/FormParserFactory.php
@@ -5,16 +5,21 @@ namespace React\Http;
 use React\Http\Parser\FormUrlencoded;
 use React\Http\Parser\Multipart;
 use React\Http\Parser\NoBody;
+use React\Http\Parser\ParserInterface;
 
 class FormParserFactory
 {
+    /**
+     * @param Request $request
+     * @return ParserInterface
+     */
     public static function create(Request $request)
     {
         $headers = $request->getHeaders();
         $headers = array_change_key_case($headers, CASE_LOWER);
 
         if (!array_key_exists('content-type', $headers)) {
-            return new NoBody();
+            return new NoBody($request);
         }
 
         $contentType = strtolower($headers['content-type']);
@@ -27,6 +32,6 @@ class FormParserFactory
             return new FormUrlencoded($request);
         }
 
-        return new NoBody();
+        return new NoBody($request);
     }
 }
