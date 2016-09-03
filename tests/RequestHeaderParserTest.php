dd966ba4f4c12a21bce4d151e00260df0458c758
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-03 18:28:56 +0200

    Testing guzzle parse exception throwing

--- tests/RequestHeaderParserTest.php
+++ tests/RequestHeaderParserTest.php
@@ -113,6 +113,22 @@ class RequestHeaderParserTest extends TestCase
         $this->assertSame('Maximum header size of 4096 exceeded.', $error->getMessage());
     }
 
+    public function testGuzzleRequestParseException()
+    {
+        $error = null;
+
+        $parser = new RequestHeaderParser();
+        $parser->on('headers', $this->expectCallableNever());
+        $parser->on('error', function ($message) use (&$error) {
+            $error = $message;
+        });
+
+        $parser->feed("\r\n\r\n");
+
+        $this->assertInstanceOf('InvalidArgumentException', $error);
+        $this->assertSame('Invalid message', $error->getMessage());
+    }
+
     private function createGetRequest()
     {
         $data = "GET / HTTP/1.1\r\n";
