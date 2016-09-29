758ad272cbb404c0a07a4e46f47ecc9da83163be
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-29 17:24:20 +0200

    Added documentation to the parser interface and added cancel method to cancel parsing of a request body stream

--- src/StreamingBodyParser/ParserInterface.php
+++ src/StreamingBodyParser/ParserInterface.php
@@ -5,7 +5,28 @@ namespace React\Http\StreamingBodyParser;
 use Evenement\EventEmitterInterface;
 use React\Http\Request;
 
+/**
+ * Parser can emit the following events:
+ *     end - When done or canceled (required)
+ *     body - Raw request body (optional)
+ *     post - Post field (optional)
+ *     file - Uploaded file (optional)
+ */
 interface ParserInterface extends EventEmitterInterface
 {
-    public function __construct(Request $request);
+    /**
+     * Factory method creating the parser.
+     *
+     * @param Request $request
+     * @return ParserInterface
+     */
+    public static function create(Request $request);
+
+    /**
+     * Cancel parsing the request body stream.
+     * Parser will still emit end event to any listeners.
+     *
+     * @return void
+     */
+    public function cancel();
 }
