304611fe74d94cc91bbb2f5ede1eb97271507f37
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-29 17:32:43 +0200

    Updated the  raw body parser to comply wth the new parser interface

--- src/StreamingBodyParser/RawBodyParser.php
+++ src/StreamingBodyParser/RawBodyParser.php
@@ -4,20 +4,35 @@ namespace React\Http\StreamingBodyParser;
 
 use Evenement\EventEmitterTrait;
 use React\Http\Request;
+use React\Promise\ExtendedPromiseInterface;
 
 class RawBodyParser implements ParserInterface
 {
     use EventEmitterTrait;
 
     /**
+     * @var ExtendedPromiseInterface
+     */
+    private $promise;
+
+    /**
+     * @param Request $request
+     * @return ParserInterface
+     */
+    public static function create(Request $request)
+    {
+        return new static($request);
+    }
+
+    /**
      * @param Request $request
      */
-    public function __construct(Request $request)
+    private function __construct(Request $request)
     {
         $headers = $request->getHeaders();
         $headers = array_change_key_case($headers, CASE_LOWER);
 
-        ContentLengthBufferedSink::createPromise(
+        $this->promise = ContentLengthBufferedSink::createPromise(
             $request,
             $headers['content-length']
         )->then([$this, 'finish']);
@@ -25,10 +40,17 @@ class RawBodyParser implements ParserInterface
 
     /**
      * @param string $buffer
+     *
+     * @internal
      */
     public function finish($buffer)
     {
         $this->emit('body', [$buffer]);
         $this->emit('end');
+    }
+
+    public function cancel()
+    {
+        $this->promise->cancel();
     }
 }
