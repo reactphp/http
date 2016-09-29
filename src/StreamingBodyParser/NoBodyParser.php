1ae613fac6e59db2a64d0f679e41eb88c7f54244
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-29 17:39:15 +0200

    No body parser

---
+++ src/StreamingBodyParser/NoBodyParser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace React\Http\StreamingBodyParser;
+
+use Evenement\EventEmitterTrait;
+use React\Http\Request;
+
+class NoBodyParser implements ParserInterface
+{
+    use EventEmitterTrait;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @param Request $request
+     * @return ParserInterface
+     */
+    public static function create(Request $request)
+    {
+        return new static($request);
+    }
+
+    /**
+     * @param Request $request
+     */
+    private function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->request->on('end', [$this, 'end']);
+    }
+
+    public function cancel()
+    {
+        $this->request->removeListener('end', [$this, 'end']);
+        $this->emit('end');
+    }
+
+    /**
+     * @internal
+     */
+    public function end()
+    {
+        $this->emit('end');
+    }
+}
