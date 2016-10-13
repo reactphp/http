1d40eb7714d38015c11ba97878771bf09eee1607
Christian Lück <christian@lueck.tv>
2016-10-13 17:14:16 +0200

    Add request header accessors (à la PSR-7)

--- src/Request.php
+++ src/Request.php
@@ -48,9 +48,65 @@ class Request extends EventEmitter implements ReadableStreamInterface
         return $this->httpVersion;
     }
 
+    /**
+     * Returns ALL headers
+     *
+     * This will return an (possibly empty) assoc array with header names as
+     * key and header values as value. The header value will be a string if
+     * there's only a single value or an array of strings if this header has
+     * multiple values.
+     *
+     * Note that this differs from the PSR-7 implementation of this method.
+     *
+     * @return array
+     */
     public function getHeaders()
     {
         return $this->headers;
+    }
+
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * @param string $name
+     * @return string[] a list of all values for this header name or an empty array if header was not found
+     */
+    public function getHeader($name)
+    {
+        $found = array();
+
+        $name = strtolower($name);
+        foreach ($this->headers as $key => $value) {
+            if (strtolower($key) === $name) {
+                foreach((array)$value as $one) {
+                    $found []= $one;
+                }
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * Retrieves a comma-separated string of the values for a single header.
+     *
+     * @param string $name
+     * @return string a comma-separated list of all values for this header name or an empty string if header was not found
+     */
+    public function getHeaderLine($name)
+    {
+        return implode(', ', $this->getHeader($name));
+    }
+
+    /**
+     * Checks if a header exists by the given case-insensitive name.
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function hasHeader($name)
+    {
+        return !!$this->getHeader($name);
     }
 
     public function expectsContinue()
