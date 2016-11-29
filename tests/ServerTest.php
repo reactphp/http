f126189040a04ae6325a9b393f614735f7359ec6
Bohdan Yurov <bohdan.yurov@accellion.com>
2016-11-29 16:04:00 +0200

    Fixes #84 issue: pause/resume on request not working

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -2,6 +2,7 @@
 
 namespace React\Tests\Http;
 
+use React\Http\Request;
 use React\Http\Server;
 
 class ServerTest extends TestCase
@@ -64,6 +65,60 @@ class ServerTest extends TestCase
         $conn->emit('data', array($data));
 
         $this->assertContains("\r\nX-Powered-By: React/alpha\r\n", $conn->getData());
+    }
+
+    public function testParserErrorEmitted()
+    {
+        $io = new ServerStub();
+
+        $error = null;
+        $server = new Server($io);
+        $server->on('headers', $this->expectCallableNever());
+        $server->on('error', function ($message) use (&$error) {
+            $error = $message;
+        });
+
+        $conn = new ConnectionStub();
+        $io->emit('connection', [$conn]);
+
+        $data = $this->createGetRequest();
+        $data = str_pad($data, 4096 * 4);
+        $conn->emit('data', [$data]);
+
+        $this->assertInstanceOf('OverflowException', $error);
+        $this->assertEquals('', $conn->getData());
+    }
+
+    /**
+     * @url https://github.com/reactphp/http/issues/84
+     */
+    public function testPauseResume()
+    {
+        $io = new ServerStub();
+
+        $server = new Server($io);
+        $called = false;
+        $server->on('request', function (Request $request) use (&$called) {
+            $called = true;
+
+            $request->emit('pause');
+            $request->emit('resume');
+        });
+
+        /** @var ConnectionStub|\PHPUnit_Framework_MockObject_MockObject $conn */
+        $conn = $this->getMock(ConnectionStub::class, ['pause', 'resume'], [], '', false, false);
+        $conn->expects($this->once())
+            ->method('pause')
+        ;
+        $conn->expects($this->once())
+            ->method('resume')
+        ;
+        $io->emit('connection', [$conn]);
+
+        $data = $this->createGetRequest();
+        $conn->emit('data', [$data]);
+
+        $this->assertTrue($called);
     }
 
     private function createGetRequest()
