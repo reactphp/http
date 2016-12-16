e6d5a1687a02dab5902dbf372fce263a55e3a13c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-16 17:25:13 +0100

    Marked RawBodyParser final, it shouldn't be extended but wrapped/decorated instead

--- src/StreamingBodyParser/RawBodyParser.php
+++ src/StreamingBodyParser/RawBodyParser.php
@@ -10,7 +10,7 @@ use React\Promise\ExtendedPromiseInterface;
 /**
  * @internal
  */
-class RawBodyParser implements ParserInterface
+final class RawBodyParser implements ParserInterface
 {
     use EventEmitterTrait;
 
