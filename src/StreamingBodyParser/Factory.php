b3436bb400e0d761fc85bcfc4caa8eb67446cbda
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-16 17:21:06 +0100

    Factory create documentation

--- src/StreamingBodyParser/Factory.php
+++ src/StreamingBodyParser/Factory.php
@@ -7,6 +7,10 @@ use React\Http\Request;
 class Factory
 {
     /**
+     * Creates the most suitable streaming body parser for the given request.
+     * It can return the following parsers:
+     *   * RawBodyParser - Emit raw body chunks as they come in. This is the default parser.
+     *
      * @param Request $request
      * @return ParserInterface
      */
