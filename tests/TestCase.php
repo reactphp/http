2e7d1e39826156dc84991c0a30ef04319ddd4d5c
Niels Theen <werwofl@googlemail.com>
2017-02-09 16:59:57 +0100

    Add new method to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -34,6 +34,17 @@ class TestCase extends \PHPUnit_Framework_TestCase
         return $mock;
     }
 
+    protected function expectCallableOnceWith($value)
+    {
+        $mock = $this->createCallableMock();
+        $mock
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($this->equalTo($value));
+
+        return $mock;
+    }
+
     protected function createCallableMock()
     {
         return $this
