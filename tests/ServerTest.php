beb143e0647da2c2a449d31107e07ae12a139ee8
Niels Theen <werwofl@googlemail.com>
2017-02-09 11:59:50 +0100

    Remove stubs from server tests

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -5,43 +5,72 @@ namespace React\Tests\Http;
 use React\Http\Server;
 use React\Http\Response;
 use React\Http\Request;
+use React\Socket\Server as Socket;
 
 class ServerTest extends TestCase
 {
+    private $connection;
+    private $loop;
+
+    public function setUp()
+    {
+        $this->loop = \React\EventLoop\Factory::create();
+
+        $this->connection = $this->getMockBuilder('React\Socket\Connection')
+            ->disableOriginalConstructor()
+            ->setMethods(
+                array(
+                    'write',
+                    'end',
+                    'close',
+                    'pause',
+                    'resume',
+                    'isReadable',
+                    'isWritable',
+                    'getRemoteAddress',
+                    'pipe'
+                )
+            )
+            ->getMock();
+    }
+
     public function testRequestEventIsEmitted()
     {
-        $io = new ServerStub();
+        $socket = new Socket($this->loop);
 
-        $server = new Server($io);
+        $server = new Server($socket);
         $server->on('request', $this->expectCallableOnce());
 
-        $conn = new ConnectionStub();
-        $io->emit('connection', array($conn));
+        $socket->emit('connection', array($this->connection));
 
         $data = $this->createGetRequest();
-        $conn->emit('data', array($data));
+        $this->connection->emit('data', array($data));
     }
 
     public function testRequestEvent()
     {
-        $io = new ServerStub();
+        $socket = new Socket($this->loop);
 
         $i = 0;
         $requestAssertion = null;
         $responseAssertion = null;
 
-        $server = new Server($io);
+        $server = new Server($socket);
         $server->on('request', function ($request, $response) use (&$i, &$requestAssertion, &$responseAssertion) {
             $i++;
             $requestAssertion = $request;
             $responseAssertion = $response;
         });
 
-        $conn = new ConnectionStub();
-        $io->emit('connection', array($conn));
+        $this->connection
+            ->expects($this->once())
+            ->method('getRemoteAddress')
+            ->willReturn('127.0.0.1');
+
+        $socket->emit('connection', array($this->connection));
 
         $data = $this->createGetRequest();
-        $conn->emit('data', array($data));
+        $this->connection->emit('data', array($data));
 
         $this->assertSame(1, $i);
         $this->assertInstanceOf('React\Http\Request', $requestAssertion);
@@ -54,43 +83,47 @@ class ServerTest extends TestCase
 
     public function testResponseContainsPoweredByHeader()
     {
-        $io = new ServerStub();
+        $socket = new Socket($this->loop);
 
-        $server = new Server($io);
+        $server = new Server($socket);
         $server->on('request', function (Request $request, Response $response) {
             $response->writeHead();
             $response->end();
         });
 
-        $conn = new ConnectionStub();
-        $io->emit('connection', array($conn));
+        $this->connection
+            ->expects($this->exactly(2))
+            ->method('write')
+            ->withConsecutive(
+                array($this->equalTo("HTTP/1.1 200 OK\r\nX-Powered-By: React/alpha\r\nTransfer-Encoding: chunked\r\n\r\n")),
+                array($this->equalTo("0\r\n\r\n"))
+            );
+
+        $socket->emit('connection', array($this->connection));
 
         $data = $this->createGetRequest();
-        $conn->emit('data', array($data));
-
-        $this->assertContains("\r\nX-Powered-By: React/alpha\r\n", $conn->getData());
+        $this->connection->emit('data', array($data));
     }
 
     public function testParserErrorEmitted()
     {
-        $io = new ServerStub();
+        $socket = new Socket($this->loop);
 
         $error = null;
-        $server = new Server($io);
+        $server = new Server($socket);
         $server->on('headers', $this->expectCallableNever());
         $server->on('error', function ($message) use (&$error) {
             $error = $message;
         });
 
-        $conn = new ConnectionStub();
-        $io->emit('connection', [$conn]);
+        $socket->emit('connection', array($this->connection));
 
         $data = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nX-DATA: ";
         $data .= str_repeat('A', 4097 - strlen($data)) . "\r\n\r\n";
-        $conn->emit('data', [$data]);
+        $this->connection->emit('data', [$data]);
 
         $this->assertInstanceOf('OverflowException', $error);
-        $this->assertEquals('', $conn->getData());
+        $this->connection->expects($this->never())->method('write');
     }
 
     private function createGetRequest()
