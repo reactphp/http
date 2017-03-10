1122c4aa7b74e08c758c0b65d609997ea435cff3
Niels Theen <werwofl@googlemail.com>
2017-03-10 11:12:58 +0100

    Return Promise with PSR-7 Response resolving

--- src/Response.php
+++ src/Response.php
@@ -2,262 +2,35 @@
 
 namespace React\Http;
 
-use Evenement\EventEmitter;
-use React\Stream\WritableStreamInterface;
+use RingCentral\Psr7\Response as Psr7Response;
+use React\Stream\ReadableStreamInterface;
+use React\Http\HttpBodyStream;
 
 /**
- * The `Response` class is responsible for streaming the outgoing response body.
- *
- * It implements the `WritableStreamInterface`.
- *
- * The constructor is internal, you SHOULD NOT call this yourself.
- * The `Server` is responsible for emitting `Request` and `Response` objects.
- *
- * The `Response` will automatically use the same HTTP protocol version as the
- * corresponding `Request`.
- *
- * HTTP/1.1 responses will automatically apply chunked transfer encoding if
- * no `Content-Length` header has been set.
- * See `writeHead()` for more details.
- *
- * See the usage examples and the class outline for details.
- *
- * @see WritableStreamInterface
- * @see Server
+ * Implementation of the PSR-7 ResponseInterface
+ * This class is an extension of RingCentral\Psr7\Response.
+ * The only difference is that this class will accept implemenations
+ * of the ReactPHPs ReadableStreamInterface for $body.
  */
-class Response extends EventEmitter implements WritableStreamInterface
+class Response extends Psr7Response
 {
-    private $conn;
-    private $protocolVersion;
-
-    private $closed = false;
-    private $writable = true;
-    private $headWritten = false;
-    private $chunkedEncoding = false;
-
-    /**
-     * The constructor is internal, you SHOULD NOT call this yourself.
-     *
-     * The `Server` is responsible for emitting `Request` and `Response` objects.
-     *
-     * Constructor parameters may change at any time.
-     *
-     * @internal
-     */
-    public function __construct(WritableStreamInterface $conn, $protocolVersion = '1.1')
-    {
-        $this->conn = $conn;
-        $this->protocolVersion = $protocolVersion;
-
-        $that = $this;
-        $this->conn->on('close', array($this, 'close'));
-
-        $this->conn->on('error', function ($error) use ($that) {
-            $that->emit('error', array($error));
-        });
-
-        $this->conn->on('drain', function () use ($that) {
-            $that->emit('drain');
-        });
-    }
-
-    public function isWritable()
-    {
-        return $this->writable;
-    }
-
-    /**
-     * Writes the given HTTP message header.
-     *
-     * This method MUST be invoked once before calling `write()` or `end()` to send
-     * the actual HTTP message body:
-     *
-     * ```php
-     * $response->writeHead(200, array(
-     *     'Content-Type' => 'text/plain'
-     * ));
-     * $response->end('Hello World!');
-     * ```
-     *
-     * Calling this method more than once will result in an `Exception`
-     * (unless the response has ended/closed already).
-     * Calling this method after the response has ended/closed is a NOOP.
-     *
-     * Unless you specify a `Content-Length` header yourself, HTTP/1.1 responses
-     * will automatically use chunked transfer encoding and send the respective header
-     * (`Transfer-Encoding: chunked`) automatically. If you know the length of your
-     * body, you MAY specify it like this instead:
-     *
-     * ```php
-     * $data = 'Hello World!';
-     *
-     * $response->writeHead(200, array(
-     *     'Content-Type' => 'text/plain',
-     *     'Content-Length' => strlen($data)
-     * ));
-     * $response->end($data);
-     * ```
-     *
-     * Note that it will automatically assume a `X-Powered-By: react/alpha` header
-     * unless your specify a custom `X-Powered-By` header yourself:
-     *
-     * ```php
-     * $response->writeHead(200, array(
-     *     'X-Powered-By' => 'PHP 3'
-     * ));
-     * ```
-     *
-     * If you do not want to send this header at all, you can use an empty array as
-     * value like this:
-     *
-     * ```php
-     * $response->writeHead(200, array(
-     *     'X-Powered-By' => array()
-     * ));
-     * ```
-     *
-     * Note that persistent connections (`Connection: keep-alive`) are currently
-     * not supported.
-     * As such, HTTP/1.1 response messages will automatically include a
-     * `Connection: close` header, irrespective of what header values are
-     * passed explicitly.
-     *
-     * @param int   $status
-     * @param array $headers
-     * @throws \Exception
-     */
-    public function writeHead($status = 200, array $headers = array())
-    {
-        if (!$this->writable) {
-            return;
-        }
-        if ($this->headWritten) {
-            throw new \Exception('Response head has already been written.');
+    public function __construct(
+        $status = 200,
+        array $headers = array(),
+        $body = null,
+        $version = '1.1',
+        $reason = null
+    ) {
+        if ($body instanceof ReadableStreamInterface) {
+            $body = new HttpBodyStream($body, null);
         }
 
-        $lower = array_change_key_case($headers);
-
-        // assign default "X-Powered-By" header as first for history reasons
-        if (!isset($lower['x-powered-by'])) {
-            $headers = array_merge(
-                array('X-Powered-By' => 'React/alpha'),
-                $headers
-            );
-        }
-
-        // always remove transfer-encoding
-        foreach($headers as $name => $value) {
-            if (strtolower($name) === 'transfer-encoding') {
-                unset($headers[$name]);
-            }
-        }
-
-        // assign date header if no 'date' is given, use the current time where this code is running
-        if (!isset($lower['date'])) {
-            // IMF-fixdate  = day-name "," SP date1 SP time-of-day SP GMT
-            $headers['Date'] = gmdate('D, d M Y H:i:s') . ' GMT';
-        }
-
-        // assign chunked transfer-encoding if no 'content-length' is given for HTTP/1.1 responses
-        if (!isset($lower['content-length']) && $this->protocolVersion === '1.1') {
-            $headers['Transfer-Encoding'] = 'chunked';
-            $this->chunkedEncoding = true;
-        }
-
-        // HTTP/1.1 assumes persistent connection support by default
-        // we do not support persistent connections, so let the client know
-        if ($this->protocolVersion === '1.1') {
-            foreach($headers as $name => $value) {
-                if (strtolower($name) === 'connection') {
-                    unset($headers[$name]);
-                }
-            }
-
-            $headers['Connection'] = 'close';
-        }
-
-        $data = $this->formatHead($status, $headers);
-        $this->conn->write($data);
-
-        $this->headWritten = true;
-    }
-
-    private function formatHead($status, array $headers)
-    {
-        $status = (int) $status;
-        $text = isset(ResponseCodes::$statusTexts[$status]) ? ResponseCodes::$statusTexts[$status] : '';
-        $data = "HTTP/$this->protocolVersion $status $text\r\n";
-
-        foreach ($headers as $name => $value) {
-            $name = str_replace(array("\r", "\n"), '', $name);
-
-            foreach ((array) $value as $val) {
-                $val = str_replace(array("\r", "\n"), '', $val);
-
-                $data .= "$name: $val\r\n";
-            }
-        }
-        $data .= "\r\n";
-
-        return $data;
-    }
-
-    public function write($data)
-    {
-        if (!$this->writable) {
-            return false;
-        }
-        if (!$this->headWritten) {
-            throw new \Exception('Response head has not yet been written.');
-        }
-
-        // prefix with chunk length for chunked transfer encoding
-        if ($this->chunkedEncoding) {
-            $len = strlen($data);
-
-            // skip empty chunks
-            if ($len === 0) {
-                return true;
-            }
-
-            $data = dechex($len) . "\r\n" . $data . "\r\n";
-        }
-
-        return $this->conn->write($data);
-    }
-
-    public function end($data = null)
-    {
-        if (!$this->writable) {
-            return;
-        }
-        if (!$this->headWritten) {
-            throw new \Exception('Response head has not yet been written.');
-        }
-
-        if (null !== $data) {
-            $this->write($data);
-        }
-
-        if ($this->chunkedEncoding) {
-            $this->conn->write("0\r\n\r\n");
-        }
-
-        $this->writable = false;
-        $this->conn->end();
-    }
-
-    public function close()
-    {
-        if ($this->closed) {
-            return;
-        }
-
-        $this->closed = true;
-        $this->writable = false;
-        $this->conn->close();
-
-        $this->emit('close');
-        $this->removeAllListeners();
+        parent::__construct(
+            $status,
+            $headers,
+            $body,
+            $version,
+            $reason
+        );
     }
 }
---
+++ tests/ChunkedEncoderTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace React\Tests\Http;
+
+use React\Stream\ReadableStream;
+use React\Http\ChunkedEncoder;
+
+class ChunkedEncoderTest extends TestCase
+{
+    private $input;
+    private $chunkedStream;
+
+    public function setUp()
+    {
+        $this->input = new ReadableStream();
+        $this->chunkedStream = new ChunkedEncoder($this->input);
+    }
+
+    public function testChunked()
+    {
+        $this->chunkedStream->on('data', $this->expectCallableOnce(array("5\r\nhello\r\n")));
+        $this->input->emit('data', array('hello'));
+    }
+
+    public function testEmptyString()
+    {
+        $this->chunkedStream->on('data', $this->expectCallableNever());
+        $this->input->emit('data', array(''));
+    }
+
+    public function testBiggerStringToCheckHexValue()
+    {
+        $this->chunkedStream->on('data', $this->expectCallableOnce(array("1a\r\nabcdefghijklmnopqrstuvwxyz\r\n")));
+        $this->input->emit('data', array('abcdefghijklmnopqrstuvwxyz'));
+    }
+
+    public function testHandleClose()
+    {
+        $this->chunkedStream->on('close', $this->expectCallableOnce());
+
+        $this->input->close();
+
+        $this->assertFalse($this->chunkedStream->isReadable());
+    }
+
+    public function testHandleError()
+    {
+        $this->chunkedStream->on('error', $this->expectCallableOnce());
+        $this->chunkedStream->on('close', $this->expectCallableOnce());
+
+        $this->input->emit('error', array(new \RuntimeException()));
+
+        $this->assertFalse($this->chunkedStream->isReadable());
+    }
+
+    public function testPauseStream()
+    {
+        $input = $this->getMockBuilder('React\Stream\ReadableStreamInterface')->getMock();
+        $input->expects($this->once())->method('pause');
+
+        $parser = new ChunkedEncoder($input);
+        $parser->pause();
+    }
+
+    public function testResumeStream()
+    {
+        $input = $this->getMockBuilder('React\Stream\ReadableStreamInterface')->getMock();
+        $input->expects($this->once())->method('pause');
+
+        $parser = new ChunkedEncoder($input);
+        $parser->pause();
+        $parser->resume();
+    }
+
+    public function testPipeStream()
+    {
+        $dest = $this->getMockBuilder('React\Stream\WritableStreamInterface')->getMock();
+
+        $ret = $this->chunkedStream->pipe($dest);
+
+        $this->assertSame($dest, $ret);
+    }
+}
