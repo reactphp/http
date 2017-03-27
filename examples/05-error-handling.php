bc4e9bc264ea8994dcec686741a209a8ff23fa31
Niels Theen <werwofl@googlemail.com>
2017-03-27 02:41:15 +0200

    Adapt examples to always returning a promise

---
+++ examples/05-error-handling.php
@@ -0,0 +1,35 @@
+<?php
+
+use React\EventLoop\Factory;
+use React\Socket\Server;
+use React\Http\Response;
+use Psr\Http\Message\RequestInterface;
+use React\Promise\Promise;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = Factory::create();
+$socket = new Server(isset($argv[1]) ? $argv[1] : '0.0.0.0:0', $loop);
+
+$count = 0;
+$server = new \React\Http\Server($socket, function (RequestInterface $request) use (&$count) {
+    return new Promise(function ($resolve, $reject) use (&$count) {
+        $count++;
+
+        if ($count%2 === 0) {
+            throw new Exception('Second call');
+        }
+
+        $response = new Response(
+            200,
+            array('Content-Type' => 'text/plain'),
+            "Hello World!\n"
+        );
+
+        $resolve($response);
+    });
+});
+
+echo 'Listening on http://' . $socket->getAddress() . PHP_EOL;
+
+$loop->run();
