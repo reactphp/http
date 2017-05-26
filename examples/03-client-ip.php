d225efb9ca4c3972cebf3c68684d8a0b90cce75b
Christian Lück <christian@lueck.tv>
2017-05-26 15:00:56 +0200

    Fix reporting listening addresses in examples

--- examples/03-client-ip.php
+++ examples/03-client-ip.php
@@ -20,6 +20,6 @@ $server = new \React\Http\Server($socket, function (ServerRequestInterface $requ
     );
 });
 
-echo 'Listening on http://' . $socket->getAddress() . PHP_EOL;
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
 
 $loop->run();
