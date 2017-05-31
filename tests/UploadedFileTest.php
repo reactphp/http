056eea404a76b7f2100a47ff679f81f5f4bd703c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-05-31 19:36:02 +0200

    Use annotations instead of expect exception methods

--- tests/UploadedFileTest.php
+++ tests/UploadedFileTest.php
@@ -21,19 +21,21 @@ class UploadedFileTest extends TestCase
 
     /**
      * @dataProvider failtyErrorProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid error code, must be an UPLOAD_ERR_* constant
      */
     public function testFailtyError($error)
     {
-        self::expectException('\InvalidArgumentException');
-        self::expectExceptionMessage('Invalid error code, must be an UPLOAD_ERR_* constant');
         $stream = new BufferStream();
         new UploadedFile($stream, 0, $error, 'foo.bar', 'foo/bar');
     }
 
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Not implemented
+     */
     public function testNoMoveFile()
     {
-        self::expectException('\RuntimeException');
-        self::expectExceptionMessage('Not implemented');
         $stream = new BufferStream();
         $uploadedFile = new UploadedFile($stream, 0, UPLOAD_ERR_OK, 'foo.bar', 'foo/bar');
         $uploadedFile->moveTo('bar.foo');
@@ -50,10 +52,12 @@ class UploadedFileTest extends TestCase
         self::assertSame('foo/bar',     $uploadedFile->getClientMediaType());
     }
 
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Cannot retrieve stream due to upload error
+     */
     public function testGetStreamOnFailedUpload()
     {
-        self::expectException('\RuntimeException');
-        self::expectExceptionMessage('Cannot retrieve stream due to upload error');
         $stream = new BufferStream();
         $uploadedFile = new UploadedFile($stream, 0, UPLOAD_ERR_NO_FILE, 'foo.bar', 'foo/bar');
         $uploadedFile->getStream();
