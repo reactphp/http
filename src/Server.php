725d1db9bc53f7e13eeedbca7c43d0c7a3bbfe75
andig <cpuidle@gmx.de>
2017-10-04 09:13:41 +0200

    Revert elvis operator

--- src/Server.php
+++ src/Server.php
@@ -186,7 +186,7 @@ class Server extends EventEmitter
 
             $that->writeError(
                 $conn,
-                $e->getCode() ?: 500
+                $e->getCode() !== 0 ? $e->getCode() : 500
             );
         });
     }
