a37b4bcc09345502020a6b2b732d6dafa4c07271
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-03 21:15:12 +0100

    Server Facade

---
+++ src/Server.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace React\Http;
+
+use React\Http\Io\IniUtil;
+use React\Http\Middleware\LimitConcurrentRequestsMiddleware;
+use React\Http\Middleware\RequestBodyBufferMiddleware;
+use React\Http\Middleware\RequestBodyParserMiddleware;
+use React\Socket\ServerInterface;
+
+/**
+ * Facade around StreamingServer with sane defaults for 80% of the use cases.
+ * The requests passed to your callable are fully compatible with PSR-7 because
+ * the body of the requests are fully buffered and parsed, unlike StreamingServer
+ * where the body is a raw ReactPHP stream.
+ *
+ * Wraps StreamingServer with the following middleware:
+ * - LimitConcurrentRequestsMiddleware
+ * - RequestBodyBufferMiddleware
+ * - RequestBodyParserMiddleware (only when enable_post_data_reading is true (default))
+ * - The callable you in passed as first constructor argument
+ *
+ * All middleware use their default configuration, which can be controlled with
+ * the the following configuration directives from php.ini:
+ * - upload_max_filesize
+ * - post_max_size
+ * - max_input_nesting_level
+ * - max_input_vars
+ * - file_uploads
+ * - max_file_uploads
+ * - enable_post_data_reading
+ */
+final class Server
+{
+    /**
+     * @internal
+     */
+    const MAXIMUM_CONCURRENT_REQUESTS = 100;
+
+    /**
+     * @var StreamingServer
+     */
+    private $streamingServer;
+
+    /**
+     * @see StreamingServer::__construct()
+     */
+    public function __construct($callback)
+    {
+        if (!is_callable($callback)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $middleware = array();
+        $middleware[] = new LimitConcurrentRequestsMiddleware($this->getConcurrentRequestsLimit());
+        $middleware[] = new RequestBodyBufferMiddleware();
+        // Checking for an empty string because that is what a boolean
+        // false is returned as by ini_get depending on the PHP version.
+        // @link http://php.net/manual/en/ini.core.php#ini.enable-post-data-reading
+        // @link http://php.net/manual/en/function.ini-get.php#refsect1-function.ini-get-notes
+        // @link https://3v4l.org/qJtsa
+        $enablePostDataReading = ini_get('enable_post_data_reading');
+        if ($enablePostDataReading !== '') {
+            $middleware[] = new RequestBodyParserMiddleware();
+        }
+        $middleware[] = $callback;
+
+        $this->streamingServer = new StreamingServer(new MiddlewareRunner($middleware));
+    }
+
+    /**
+     * @see StreamingServer::listen()
+     */
+    public function listen(ServerInterface $server)
+    {
+        $this->streamingServer->listen($server);
+    }
+
+    private function getConcurrentRequestsLimit()
+    {
+        if (ini_get('memory_limit') == -1) {
+            return self::MAXIMUM_CONCURRENT_REQUESTS;
+        }
+
+        $availableMemory = IniUtil::iniSizeToBytes(ini_get('memory_limit')) / 4;
+        $concurrentRequests = $availableMemory / IniUtil::iniSizeToBytes(ini_get('post_max_size'));
+        $concurrentRequests = ceil($concurrentRequests);
+
+        if ($concurrentRequests >= self::MAXIMUM_CONCURRENT_REQUESTS) {
+            return self::MAXIMUM_CONCURRENT_REQUESTS;
+        }
+
+        return $concurrentRequests;
+    }
+}
