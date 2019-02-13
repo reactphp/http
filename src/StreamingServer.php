dd9630ea3524c92799d038ff51e05bd48a60acdb
woytam <woytam@users.noreply.github.com>
2019-02-13 17:21:48 +0100

    Add verify_peer SSL context in comments
    For standard HTTPS server, it is necessary to disable peer verification, which is only used when client are authenticated by client certificates. HTTPS is used only server certificates.

--- src/StreamingServer.php
+++ src/StreamingServer.php
@@ -139,7 +139,9 @@ final class StreamingServer extends EventEmitter
      *
      * $socket = new React\Socket\Server(8080, $loop);
      * $socket = new React\Socket\SecureServer($socket, $loop, array(
-     *     'local_cert' => __DIR__ . '/localhost.pem'
+     *     'local_cert' => __DIR__ . '/localhost.pem',
+     *     'verify_peer' => false,         // Used to authenticate the identity of the remote peer (client)
+     *     'verify_peer_name' => false     // Used to authenticate the identity of the remote peer (client)
      * ));
      *
      * $server->listen($socket);
