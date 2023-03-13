c07af0dd57be0463bcfe1e48635c29657d7df2bd
Marcel Kempf <marcel.kempf@check24.de>
2023-03-13 14:56:02 +0100

    Use FQCN as string

--- tests/Browser/MiddlewareRunnerTest.php
+++ tests/Browser/MiddlewareRunnerTest.php
@@ -18,7 +18,7 @@ final class MiddlewareRunnerTest extends TestCase
         $request = new Request('GET', 'https://example.com/');
 
         /** @var Transaction&MockObject $transaction */
-        $transaction = $this->createMock(Transaction::class);
+        $transaction = $this->createMock('\React\Http\Io\Transaction');
         $response = new Response();
         $transaction->expects(static::once())->method('send')->with($request)->willReturn($response);
 
@@ -32,7 +32,7 @@ final class MiddlewareRunnerTest extends TestCase
     public function testWithMiddleware()
     {
         /** @var Transaction&MockObject $transaction */
-        $transaction = $this->createMock(Transaction::class);
+        $transaction = $this->createMock('\React\Http\Io\Transaction');
         $transaction->expects(static::never())->method('send');
 
         $response = new Response();
